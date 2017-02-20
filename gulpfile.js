e25e7f88bf1863d85ed95cc6b14c0b910ae680f5
Jakob Pupke <jakob.pupke@gmail.com>
2017-02-21 00:14:35 +0100

    eslint remove unused gulp-watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@
 const gulp = require('gulp')
 const config = require('./build/config.js')
 const requireDir = require('require-dir')
-const watch = require('gulp-watch')
 requireDir('./build', { recurse: true })
 
 gulp.task('default', () => {
