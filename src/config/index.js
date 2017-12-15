2ccd3ac7e52ea55a2571761e3f1ebffa13e65e98
Sergei <se-panfilov@ya.ru>
2017-12-15 21:49:37 +0800

    fixes for production

--- src/config/index.js
+++ src/config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'https://se-panfilov.github.io/vue-notifications/',
 
     /**
      * Source Maps
