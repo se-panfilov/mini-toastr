6d75769f986f67affb390af721e4459ed7fe25a1
Sergei Panfilov <sergei.panfilov@powerhouse.nl>
2018-05-03 12:49:27 +0200

    rollback deleted files

---
+++ test/mini-toastr.test.ts
@@ -0,0 +1,11 @@
+import DummyClass from '../src/mini-toastr'
+
+describe('Dummy test', () => {
+  it('works if true is truthy', () => {
+    expect(true).toBeTruthy()
+  })
+
+  it('DummyClass is instantiable', () => {
+    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  })
+})
